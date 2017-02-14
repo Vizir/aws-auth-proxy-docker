e7d2a16b72c1cd854f85953f7c9df829e561d3a6
cartolari <bruno.cartolari@gmail.com>
2017-02-14 19:52:27 -0200

    Fix port address

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -xeuo pipefail
 exec aws-auth-proxy \
   -access-key="$AWS_ACCESS_KEY_ID" \
   -secret-key="$AWS_SECRET_ACCESS_KEY" \
-  -listen-address=":3001" \
+  -listen-address=":3000" \
   -region-name="$AWS_REGION" \
   -service-name="$SERVICE_NAME" \
   -upstream-host="$ES_HOST" \
